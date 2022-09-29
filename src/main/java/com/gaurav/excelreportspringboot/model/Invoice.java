7ff68aaae64590a08a0d2ae567721dbef78bd711
GAURAV KUMAR <gaurav9142@hotmail.com>
2022-09-29 23:08:53 +0530

    Removed Lombok uses

--- src/main/java/com/gaurav/excelreportspringboot/model/Invoice.java
+++ src/main/java/com/gaurav/excelreportspringboot/model/Invoice.java
@@ -9,9 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Invoice {
 
     @Id
@@ -20,4 +17,56 @@ public class Invoice {
     private String name;
     private String location;
     private Double amount;
+
+    public Invoice() {
+    }
+
+    public Invoice(Long id, String name, String location, Double amount) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
